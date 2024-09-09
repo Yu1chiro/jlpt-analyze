83e11bbd0439bcc5c4825bc1ec426488a1a8d40a
HeraBeginners25 <gallerysingaraja8@gmail.com>
2024-09-09 18:45:14 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,156 @@
+const _0x3df6f4 = _0x3936;
+(function (_0x36b50f, _0x4d00be) {
+  const _0x939a28 = _0x3936,
+    _0x4a3f49 = _0x36b50f();
+  while (!![]) {
+    try {
+      const _0x317e08 =
+        (parseInt(_0x939a28(0x1e3)) / 0x1) * (-parseInt(_0x939a28(0x1ce)) / 0x2) +
+        (-parseInt(_0x939a28(0x1db)) / 0x3) * (-parseInt(_0x939a28(0x1e9)) / 0x4) +
+        (parseInt(_0x939a28(0x1ed)) / 0x5) * (-parseInt(_0x939a28(0x1d2)) / 0x6) +
+        parseInt(_0x939a28(0x1be)) / 0x7 +
+        (parseInt(_0x939a28(0x1e4)) / 0x8) * (parseInt(_0x939a28(0x1c5)) / 0x9) +
+        (parseInt(_0x939a28(0x1f5)) / 0xa) * (parseInt(_0x939a28(0x1dd)) / 0xb) +
+        parseInt(_0x939a28(0x1de)) / 0xc;
+      if (_0x317e08 === _0x4d00be) break;
+      else _0x4a3f49["push"](_0x4a3f49["shift"]());
+    } catch (_0x38b3d2) {
+      _0x4a3f49["push"](_0x4a3f49["shift"]());
+    }
+  }
+})(_0x4375, 0xa3f36);
+function _0x3936(_0x30d0b8, _0x5e1753) {
+  const _0x43757e = _0x4375();
+  return (
+    (_0x3936 = function (_0x39363b, _0x191802) {
+      _0x39363b = _0x39363b - 0x1bb;
+      let _0x472efc = _0x43757e[_0x39363b];
+      return _0x472efc;
+    }),
+    _0x3936(_0x30d0b8, _0x5e1753)
+  );
+}
+function _0x4375() {
+  const _0x217f5c = [
+    "4754bLcoeg",
+    "8YgQthN",
+    "scrollTo",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20(漢字.文字語彙.文法)\x20Vocabulary/Grammar\x20Score:\x20",
+    "replace",
+    "Error:",
+    "2725808GMATRE",
+    "value",
+    "<h3>$1</h3>",
+    "click",
+    "20lRUQbx",
+    "Please\x20complete\x20all\x20fields\x20before\x20analyzing!",
+    "Failed\x20to\x20analyze\x20the\x20scores.",
+    "text",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20Sebagai\x20analis\x20JLPT\x20profesional,\x20berikan\x20analisis\x20mendalam\x20terhadap\x20hasil\x20tryout\x20JLPT\x20berikut,\x20dengan\x20memperhatikan\x20passing\x20grade\x20untuk\x20masing-masing\x20level\x20yang\x20sesuai\x20dengan\x20ketentuan\x20Japan\x20Foundation:\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20Total\x20minimal\x20Passing\x20Grade:\x0a\x20\x20\x20\x20\x20\x20\x20\x20-\x20N2\x20minimal:\x2090\x20poin\x0a\x20\x20\x20\x20\x20\x20\x20\x20-\x20N3\x20minimal:\x2090\x20poin\x0a\x20\x20\x20\x20\x20\x20\x20\x20-\x20N4\x20minimal:\x2095\x20poin\x0a\x20\x20\x20\x20\x20\x20\x20\x20-\x20N5\x20minimal:\x2090\x20poin\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20Instruksi\x20analisis:\x0a\x20\x20\x20\x20\x20\x20\x20\x201.\x20Hitung\x20total\x20skor\x20dengan\x20menjumlahkan\x20Vocabulary/Grammar\x20Score,\x20Reading\x20Score,\x20dan\x20Listening\x20Score.\x0a\x20\x20\x20\x20\x20\x20\x20\x202.\x20Tentukan\x20status\x20kelulusan\x20dengan\x20mengikuti\x20aturan\x20berikut\x20tanpa\x20pengecualian:\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Jika\x20level\x20adalah\x20N4\x20dan\x20total\x20skor\x20>=\x2095,\x20status\x20adalah\x20LULUS\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Jika\x20level\x20adalah\x20N4\x20dan\x20total\x20skor\x20<\x2095,\x20status\x20adalah\x20TIDAK\x20LULUS\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Jika\x20level\x20adalah\x20N2,\x20N3,\x20atau\x20N5\x20dan\x20total\x20skor\x20>=\x2090,\x20status\x20adalah\x20LULUS\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Jika\x20level\x20adalah\x20N2,\x20N3,\x20atau\x20N5\x20dan\x20total\x20skor\x20<\x2090,\x20status\x20adalah\x20TIDAK\x20LULUS\x0a\x20\x20\x20\x20\x20\x20\x20\x203.\x20Berikan\x20evaluasi\x20terperinci\x20untuk\x20setiap\x20bidang\x20yang\x20diuji\x20(Vocabulary/Grammar,\x20Reading,\x20Listening).\x0a\x20\x20\x20\x20\x20\x20\x20\x204.\x20Untuk\x20setiap\x20bidang,\x20berikan\x20saran\x20dan\x20rekomendasi\x20yang\x20jelas\x20dan\x20profesional\x20untuk\x20mempertahankan\x20atau\x20meningkatkan\x20performa.\x0a\x20\x20\x20\x20\x20\x20\x20\x205.\x20Jika\x20status\x20LULUS,\x20berikan\x20dorongan\x20dan\x20keyakinan\x20kepada\x20pengguna\x20untuk\x20melanjutkan\x20ke\x20ujian\x20JLPT\x20resmi.\x0a\x20\x20\x20\x20\x20\x20\x20\x206.\x20Jika\x20status\x20TIDAK\x20LULUS,\x20berikan\x20motivasi\x20dan\x20strategi\x20untuk\x20meningkatkan\x20skor.\x0a\x20\x20\x20\x20\x20\x20\x20\x207.\x20Berikan\x20rekomendasi\x20umum\x20untuk\x20persiapan\x20ujian\x20JLPT\x20selanjutnya.\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20Skor\x20Try\x20out\x20JLPT\x20user\x20:\x0a\x20\x20\x20\x20\x20\x20\x20\x20Level:\x20",
+    "Waiting\x20analysis\x20✨✨✨✨",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=",
+    "Reading",
+    "40cKIabw",
+    "candidates",
+    "close",
+    "<br>",
+    "error",
+    "<h2>$1</h2>",
+    "display",
+    "436632YSdxSi",
+    "addEventListener",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20(読解)\x20Reading\x20Score:\x20",
+    "outputText",
+    "innerHTML",
+    "json",
+    "submit-analyze",
+    "539361OaUfIc",
+    "modal-output",
+    "scrollHeight",
+    "style",
+    "smooth",
+    "An\x20unexpected\x20error\x20occurred.",
+    "trim",
+    "category",
+    "<b>$1</b>",
+    "82JpICVk",
+    "fire",
+    "showLoading",
+    "Error",
+    "1018986calvcA",
+    "message",
+    ".\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20(聴解)\x20Listening\x20Score:\x20",
+    "application/json",
+    "getElementById",
+    "log",
+    "<pre><code>$1</code></pre>",
+    "stringify",
+    "3zoDfaV",
+    "AIzaSyCbENma_1hhoo4cp1ptCP4DmXapdXzwXzQ",
+    "1975897QaHbUV",
+    "282180zHIhNo",
+    "On\x20Process\x20✨✨",
+    "body",
+    "POST",
+    "<blockquote>$1</blockquote>",
+  ];
+  _0x4375 = function () {
+    return _0x217f5c;
+  };
+  return _0x4375();
+}
+function formatText(_0x492b1e) {
+  const _0x73159b = _0x3936;
+  return (
+    (_0x492b1e = _0x492b1e["replace"](/\*\*(.*?)\*\*/g, _0x73159b(0x1cd))),
+    (_0x492b1e = _0x492b1e["replace"](/\*(.*?)\*/g, "<i>$1</i>")),
+    (_0x492b1e = _0x492b1e[_0x73159b(0x1e7)](/~~(.*?)~~/g, "<s>$1</s>")),
+    (_0x492b1e = _0x492b1e["replace"](/^#\s+(.*)/gm, "<h1>$1</h1>")),
+    (_0x492b1e = _0x492b1e[_0x73159b(0x1e7)](/^##\s+(.*)/gm, _0x73159b(0x1bc))),
+    (_0x492b1e = _0x492b1e[_0x73159b(0x1e7)](/^###\s+(.*)/gm, _0x73159b(0x1eb))),
+    (_0x492b1e = _0x492b1e["replace"](/^>\s+(.*)/gm, _0x73159b(0x1e2))),
+    (_0x492b1e = _0x492b1e[_0x73159b(0x1e7)](/```(.*?)```/gs, _0x73159b(0x1d9))),
+    (_0x492b1e = _0x492b1e[_0x73159b(0x1e7)](/\n(?!<\/?(h1|h2|h3|blockquote|pre|code)>)/g, _0x73159b(0x1f8))),
+    _0x492b1e
+  );
+}
+document[_0x3df6f4(0x1d7)](_0x3df6f4(0x1c4))[_0x3df6f4(0x1bf)](_0x3df6f4(0x1ec), async function (_0x2562d8) {
+  const _0x904aa9 = _0x3df6f4;
+  _0x2562d8["preventDefault"]();
+  const _0x13e8a0 = document[_0x904aa9(0x1d7)](_0x904aa9(0x1cc))[_0x904aa9(0x1ea)]["trim"](),
+    _0x536cb0 = document[_0x904aa9(0x1d7)]("Vocalbullary")[_0x904aa9(0x1ea)][_0x904aa9(0x1cb)](),
+    _0x2e22f2 = document[_0x904aa9(0x1d7)](_0x904aa9(0x1f4))[_0x904aa9(0x1ea)][_0x904aa9(0x1cb)](),
+    _0x8fdf37 = document[_0x904aa9(0x1d7)]("Listening")[_0x904aa9(0x1ea)][_0x904aa9(0x1cb)]();
+  if (_0x13e8a0 === "" || _0x536cb0 === "" || _0x2e22f2 === "" || _0x8fdf37 === "") {
+    Swal[_0x904aa9(0x1cf)]({ icon: "error", title: "Oops...", text: _0x904aa9(0x1ee) });
+    return;
+  }
+  Swal[_0x904aa9(0x1cf)]({
+    title: _0x904aa9(0x1df),
+    text: _0x904aa9(0x1f2),
+    allowOutsideClick: ![],
+    didOpen: () => {
+      const _0x38fc01 = _0x904aa9;
+      Swal[_0x38fc01(0x1d0)]();
+    },
+  });
+  try {
+    const _0x51a2ae = _0x904aa9(0x1dc),
+      _0x174255 = _0x904aa9(0x1f1) + _0x13e8a0 + _0x904aa9(0x1e6) + _0x536cb0 + _0x904aa9(0x1c0) + _0x2e22f2 + _0x904aa9(0x1d5) + _0x8fdf37 + _0x904aa9(0x1d4),
+      _0x39efe2 = await fetch(_0x904aa9(0x1f3) + _0x51a2ae, { method: _0x904aa9(0x1e1), headers: { "Content-Type": _0x904aa9(0x1d6) }, body: JSON[_0x904aa9(0x1da)]({ contents: [{ parts: [{ text: _0x174255 }] }] }) }),
+      _0x91b7c1 = await _0x39efe2[_0x904aa9(0x1c3)]();
+    console[_0x904aa9(0x1d8)](_0x91b7c1);
+    if (_0x39efe2["ok"]) {
+      const _0x55ef93 = _0x91b7c1[_0x904aa9(0x1f6)][0x0]["content"]["parts"][0x0][_0x904aa9(0x1f0)];
+      Swal[_0x904aa9(0x1f7)]();
+      const _0x53c682 = formatText(_0x55ef93);
+      (document["getElementById"](_0x904aa9(0x1c1))[_0x904aa9(0x1c2)] = _0x53c682), (document["getElementById"](_0x904aa9(0x1c6))[_0x904aa9(0x1c8)][_0x904aa9(0x1bd)] = "block");
+      const _0x374ca4 = 0x50,
+        _0x348230 = (document[_0x904aa9(0x1e0)][_0x904aa9(0x1c7)] * _0x374ca4) / 0x64;
+      window[_0x904aa9(0x1e5)]({ top: _0x348230, behavior: _0x904aa9(0x1c9) });
+    } else throw new Error(_0x91b7c1[_0x904aa9(0x1bb)]?.[_0x904aa9(0x1d3)] || _0x904aa9(0x1ef));
+  } catch (_0x2370f6) {
+    console[_0x904aa9(0x1bb)](_0x904aa9(0x1e8), _0x2370f6), Swal[_0x904aa9(0x1cf)]({ icon: "error", title: _0x904aa9(0x1d1), text: _0x2370f6["message"] || _0x904aa9(0x1ca) });
+  }
+});
